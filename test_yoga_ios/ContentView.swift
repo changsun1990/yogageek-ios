7d9494a019045f33d603160734ea621dba198e33
Chang Liu <9713593+chang-l@users.noreply.github.com>
2026-01-12 14:15:59 -0800

    Initial Commit

---
+++ test_yoga_ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test_yoga_ios
+//
+//  Created by Chang Liu on 1/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test_yoga_ios/test_yoga_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_yoga_iosApp.swift
+//  test_yoga_ios
+//
+//  Created by Chang Liu on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct test_yoga_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
